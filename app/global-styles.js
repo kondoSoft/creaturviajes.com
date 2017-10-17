5f1a2690250959cc78053b42201a4924fd0a6c16
JoJoCoJo <jojocojo@msn.com>
2017-10-17 17:02:29 -0500

    Added fonts for proyect

--- app/global-styles.js
+++ app/global-styles.js
@@ -1,13 +1,41 @@
 import { injectGlobal } from 'styled-components';
+import AvenirBook from '!file-loader!./assets/fonts/AvenirLTStd-Book.otf';
+import AvenirLight from '!file-loader!./assets/fonts/AvenirLTStd-Light.otf';
+import AvenirRoman from '!file-loader!./assets/fonts/AvenirLTStd-Roman.otf';
+import AntroVectra from '!file-loader!./assets/fonts/Antro_Vectra.otf';
+
+
 
 /* eslint no-unused-expressions: 0 */
 injectGlobal`
+
+  @font-face {
+    font-family: AvenirBook;
+    src: url(${AvenirBook});
+  }
+
+  @font-face {
+    font-family: AvenirLight;
+    src: url(${AvenirLight});
+  }
+  @font-face {
+    font-family: AvenirRoman;
+    src: url(${AvenirRoman});
+  }
+  /* Fuente para el banner */
+  @font-face {
+    font-family: AntroVectra;
+    src: url(${AntroVectra});
+  }
+
   html,
   body {
     height: 100%;
     width: 100%;
   }
-
+  h1{
+    font-family: AntroVectra;
+  }
   body {
     font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
   }
