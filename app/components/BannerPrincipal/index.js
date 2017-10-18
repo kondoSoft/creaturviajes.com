4256d74647fb400d159dbd8f3e563a05133712e5
JoJoCoJo <jojocojo@msn.com>
2017-10-18 14:33:22 -0500

    Created banner component

---
+++ app/components/BannerPrincipal/index.js
@@ -0,0 +1,22 @@
+/**
+*
+* BannerPrincipal
+*
+*/
+
+import React from 'react';
+// import styled from 'styled-components';
+
+
+function BannerPrincipal() {
+  return (
+    <div>
+    </div>
+  );
+}
+
+BannerPrincipal.propTypes = {
+
+};
+
+export default BannerPrincipal;
---
+++ app/components/BannerPrincipal/tests/index.test.js
@@ -0,0 +1,10 @@
+// import React from 'react';
+// import { shallow } from 'enzyme';
+
+// import BannerPrincipal from '../index';
+
+describe('<BannerPrincipal />', () => {
+  it('Expect to have unit tests specified', () => {
+    expect(true).toEqual(false);
+  });
+});
