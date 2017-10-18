6686e9e8f0bfdff4174d01c05626e89416a7a1cd
JoJoCoJo <jojocojo@msn.com>
2017-10-18 17:01:22 -0500

    Created main banner

--- app/components/BannerPrincipal/index.js
+++ app/components/BannerPrincipal/index.js
@@ -5,12 +5,16 @@
 */
 
 import React from 'react';
-// import styled from 'styled-components';
-
+import { ImgPrincipal, TituloBanner, ButtonBanner } from './styled.js';
+import ImgBannerPrincipal from '../../assets/ImagenesPruebaNuevoDiseniov2/marruecos.jpg';
 
 function BannerPrincipal() {
   return (
     <div>
+    	<ImgPrincipal src={ImgBannerPrincipal} alt='Banner Principal'/>
+    	<TituloBanner>Descubre Marruecos</TituloBanner>
+    	<ButtonBanner>Ir a programa</ButtonBanner>
+
     </div>
   );
 }
---
+++ app/components/BannerPrincipal/styled.js
@@ -0,0 +1,46 @@
+import styled from 'styled-components';
+
+const ImgPrincipal = styled.img`
+	width: 100%;
+	margin: 0px;
+	padding: 0px;
+`;
+
+const ButtonBanner = styled.button`
+  font-family: AvenirRoman;
+  display: inline-block;
+  font-weight: normal;
+  text-align: center;
+  white-space: nowrap;
+  vertical-align: middle;
+  -webkit-user-select: none;
+     -moz-user-select: none;
+      -ms-user-select: none;
+          user-select: none;
+  border: 1px solid transparent;
+  padding: 1rem 2.5rem 1rem 2.5rem;
+  font-size: 1.5rem;
+  line-height: 1.25;
+  transition: all 0.15s ease-in-out;
+  color: #fff;
+background: #ee7202;
+border-color: #ee7202;
+position: absolute;
+left: 22%;
+bottom: 25%;
+`;
+
+
+
+const TituloBanner = styled.p`
+	font-family: AntroVectra;
+	font-weight: bold;
+	font-size: 4rem;
+	color: #001a5a;
+	position: absolute;
+	left: 12%;
+	bottom: 25%;
+
+`;
+
+export { ImgPrincipal, TituloBanner, ButtonBanner }
