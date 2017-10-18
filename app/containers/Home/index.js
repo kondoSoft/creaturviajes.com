d9a42457dd1da7a0febc6b766bcac07142c49cc0
JoJoCoJo <jojocojo@msn.com>
2017-10-18 17:02:00 -0500

    Added banner principal

--- app/containers/Home/index.js
+++ app/containers/Home/index.js
@@ -10,14 +10,15 @@ import Helmet from 'react-helmet';
 import { createStructuredSelector } from 'reselect';
 import makeSelectHome from './selectors';
 import Menu from '../../components/Menu';
+import BannerPrincipal from '../../components/BannerPrincipal';
 
 
 export class Home extends React.Component { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
       <div>
-      <Menu/>
-      <h1>Hola</h1>
+        <Menu/>
+        <BannerPrincipal/>
       </div>
     );
   }
