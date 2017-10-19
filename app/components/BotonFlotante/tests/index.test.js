a361c7023d2158578cde475f475c4e726c49fe5c
JoJoCoJo <jojocojo@msn.com>
2017-10-19 09:56:04 -0500

    Added float button for the project

---
+++ app/components/BotonFlotante/tests/index.test.js
@@ -0,0 +1,10 @@
+// import React from 'react';
+// import { shallow } from 'enzyme';
+
+// import BotonFlotante from '../index';
+
+describe('<BotonFlotante />', () => {
+  it('Expect to have unit tests specified', () => {
+    expect(true).toEqual(false);
+  });
+});
