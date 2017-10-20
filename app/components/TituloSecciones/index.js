907eb177109ea0ca42c88be69320c4abb6df0864
JoJoCoJo <jojocojo@msn.com>
2017-10-20 10:23:08 -0500

    Created Title Section component

---
+++ app/components/TituloSecciones/index.js
@@ -0,0 +1,22 @@
+/**
+*
+* TituloSecciones
+*
+*/
+
+import React from 'react';
+// import styled from 'styled-components';
+
+
+function TituloSecciones() {
+  return (
+    <div>
+    </div>
+  );
+}
+
+TituloSecciones.propTypes = {
+
+};
+
+export default TituloSecciones;
---
+++ app/components/TituloSecciones/tests/index.test.js
@@ -0,0 +1,10 @@
+// import React from 'react';
+// import { shallow } from 'enzyme';
+
+// import TituloSecciones from '../index';
+
+describe('<TituloSecciones />', () => {
+  it('Expect to have unit tests specified', () => {
+    expect(true).toEqual(false);
+  });
+});
