2b06dd9f77def5ad258f12c3e9ad4c1d6a681cc7
JoJoCoJo <jojocojo@msn.com>
2017-10-25 11:04:16 -0500

    Created component section

---
+++ app/components/SeccionOportunidades/index.js
@@ -0,0 +1,22 @@
+/**
+*
+* SeccionOportunidades
+*
+*/
+
+import React from 'react';
+
+
+function SeccionOportunidades() {
+  return (
+    <div>
+    <h1>hola</h1>
+    </div>
+  );
+}
+
+SeccionOportunidades.propTypes = {
+
+};
+
+export default SeccionOportunidades;
---
+++ app/components/SeccionOportunidades/tests/index.test.js
@@ -0,0 +1,10 @@
+// import React from 'react';
+// import { shallow } from 'enzyme';
+
+// import SeccionOportunidades from '../index';
+
+describe('<SeccionOportunidades />', () => {
+  it('Expect to have unit tests specified', () => {
+    expect(true).toEqual(false);
+  });
+});
