8a93a42d9f2b27b317baf57654d4654ffe9b341f
JoJoCoJo <jojocojo@msn.com>
2017-10-25 14:38:11 -0500

    Fished v1 Section Opportunities

--- app/components/SeccionOportunidades/index.js
+++ app/components/SeccionOportunidades/index.js
@@ -6,12 +6,18 @@
 
 import React from 'react';
 import Card from '../Card';
+import { DivOportunidades } from './styled'
+import Crucero from '../../assets/ImagenesPruebaNuevoDiseniov2/crucero.jpg';
+import Temporada2 from '../../assets/ImagenesPruebaNuevoDiseniov2/temporada2.jpg';
+import Oportunidad3 from '../../assets/ImagenesPruebaNuevoDiseniov2/oportunidad3.jpg';
 
 function SeccionOportunidades() {
   return (
-    <div>
-    <Card/>
-    </div>
+ 	<DivOportunidades>
+    	<Card ImgCard={Crucero} TextoBoton='Oferta' TituloCard='MES DEL CRUCERO' TextoCuerpo='Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' />
+    	<Card ImgCard={Temporada2} TextoBoton='Fin de año' TituloCard='WHISTLER' TextoCuerpo='Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' />
+    	<Card ImgCard={Oportunidad3} TextoBoton='Nuevo' TituloCard='SINGAPUR Y MYANMAR' TextoCuerpo='Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' />
+    </DivOportunidades>
   );
 }
 
--- app/components/SeccionOportunidades/styled.js
+++ app/components/SeccionOportunidades/styled.js
@@ -1,1 +1,12 @@
 import styled from 'styled-components';
+
+const DivOportunidades = styled.div`
+	width: 100%;
+	background-color: #f5f4f4;
+	display: flex;
+	justify-content: space-evenly;
+	padding-top: 3%;
+	padding-bottom: 3%;
+`;
+
+export { DivOportunidades }
