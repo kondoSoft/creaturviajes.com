69cfd6eb2b14b3d1ea21e7f1c829c9b94ee7587f
JoJoCoJo <jojocojo@msn.com>
2017-10-26 12:46:19 -0500

    Created footer section

---
+++ app/components/PieDePagina/index.js
@@ -0,0 +1,22 @@
+/**
+*
+* PieDePagina
+*
+*/
+
+import React from 'react';
+import {Footer} from './styled.js';
+
+function PieDePagina() {
+  return (
+    <Footer>
+    	
+    </Footer>
+  );
+}
+
+PieDePagina.propTypes = {
+
+};
+
+export default PieDePagina;
---
+++ app/components/PieDePagina/styled.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Footer = styled.div`
+	right: 0px;
+	bottom: 0px;
+	left: 0px;
+	padding: 1rem;
+	background: #001a5a;
+	width: 100%;
+`;
+
+export {Footer}
