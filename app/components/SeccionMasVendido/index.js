6d105e9d790f9b9e644c00dea1a3bacba7e07a07
JoJoCoJo <jojocojo@msn.com>
2017-10-26 11:14:35 -0500

    Created component best seller

---
+++ app/components/SeccionMasVendido/index.js
@@ -0,0 +1,22 @@
+/**
+*
+* SeccionMasVendido
+*
+*/
+
+import React from 'react';
+import { DivMasVendido } from './styled'
+
+function SeccionMasVendido() {
+  return (
+    <DivMasVendido>
+    <h1>Hola</h1>
+    </DivMasVendido>
+  );
+}
+
+SeccionMasVendido.propTypes = {
+
+};
+
+export default SeccionMasVendido;
---
+++ app/components/SeccionMasVendido/tests/index.test.js
@@ -0,0 +1,10 @@
+// import React from 'react';
+// import { shallow } from 'enzyme';
+
+// import SeccionMasVendido from '../index';
+
+describe('<SeccionMasVendido />', () => {
+  it('Expect to have unit tests specified', () => {
+    expect(true).toEqual(false);
+  });
+});
