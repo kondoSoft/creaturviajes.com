1b16adf616e8e68584663ff436252e3fb7d387a7
JoJoCoJo <jojocojo@msn.com>
2017-10-26 18:04:11 -0500

    Started Footer Structure and styling

--- app/components/PieDePagina/index.js
+++ app/components/PieDePagina/index.js
@@ -5,12 +5,35 @@
 */
 
 import React from 'react';
-import {Footer} from './styled.js';
+import { 
+		Footer,
+		ColumnaPrincipalFooter,
+		ColumnaSecundaria,
+		LogoFooter,
+		EspacioCorreo,
+		NombreCorreoFooter,
+		DominioCorreoFooter
+	} from './styled.js';
+
+import ImgLogo from '!file-loader!../../assets/ImagenesPruebaNuevoDiseniov2/logo_calado.png';
 
 function PieDePagina() {
   return (
     <Footer>
+    	<ColumnaPrincipalFooter>
+    		<LogoFooter src={ImgLogo} alt='Logo Creatur' /> 
+    		
+    		<EspacioCorreo>
+	    		<NombreCorreoFooter>info</NombreCorreoFooter>
+	    		<DominioCorreoFooter>@creaturviajes.com</DominioCorreoFooter>
+	    	</EspacioCorreo>
+    	</ColumnaPrincipalFooter>
+    	<ColumnaSecundaria>
     	
+    	</ColumnaSecundaria>
+    	<ColumnaSecundaria>
+    	
+    	</ColumnaSecundaria>
     </Footer>
   );
 }
--- app/components/PieDePagina/styled.js
+++ app/components/PieDePagina/styled.js
@@ -1,12 +1,59 @@
 import styled from 'styled-components';
 
 const Footer = styled.div`
+	width: 100%;
+	display: -webkit-flex;
+	display: flex;
+	flex-direction: row;
+	flex-flow: wrap;
 	right: 0px;
 	bottom: 0px;
 	left: 0px;
-	padding: 1rem;
+	padding: 1%;
+	margin: 0;
 	background: #001a5a;
-	width: 100%;
 `;
 
-export {Footer}
+const ColumnaPrincipalFooter = styled.div`
+	width: 40%;
+	display: -webkit-flex;
+	display: flex;
+	flex-direction: column;
+	padding-left: 3%;
+`;
+
+const EspacioCorreo = styled.div`
+	margin-top: 50px;
+	display: flex;
+	flex-flow: wrap;
+`;
+
+const NombreCorreoFooter = styled.h3`
+	color: #ee7202;
+	paddig: 0;
+	margin: 0;
+`;
+
+const DominioCorreoFooter = styled(NombreCorreoFooter)`
+	color: #ffffff;
+`;
+
+const ColumnaSecundaria = styled(ColumnaPrincipalFooter)`
+	width: 30%;
+`;
+
+const LogoFooter = styled.img`
+  width: 30%;
+  margin: 0px;
+  padding: 0px;
+`;
+
+export {
+		Footer,
+		ColumnaPrincipalFooter,
+		ColumnaSecundaria,
+		LogoFooter,
+		EspacioCorreo,
+		NombreCorreoFooter,
+		DominioCorreoFooter
+	}
