1d95a54a1c59158b8bf8463a37e4b84c0178d53f
JoJoCoJo <jojocojo@msn.com>
2017-10-26 11:15:29 -0500

    Finished v1 Section type travel

---
+++ app/components/SeccionTipoDeViaje/styled.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const DivTipoDeViaje = styled.div`
+	width: 100%;
+	background-color: #ffffff;
+	display: flex;
+	justify-content: space-evenly;
+	padding-top: 3%;
+	padding-bottom: 3%;
+`;
+
+export { DivTipoDeViaje }
