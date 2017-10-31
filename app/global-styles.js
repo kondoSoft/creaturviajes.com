bdb70763c72a9c7ee0063dea486abcd71c885158
JoJoCoJo <jojocojo@msn.com>
2017-10-31 16:50:07 -0600

    Added media queries for responsive design

--- app/global-styles.js
+++ app/global-styles.js
@@ -54,24 +54,52 @@ injectGlobal`
     line-height: 1.5em;
   }
 
-
-  @media (max-width: 575px) {
-
-  }
-
-  @media (max-width: 767px) {
+  /* xsmall-0 */
+  @media screen and (max-width: 480px) {
 
   }
 
-  @media (max-width: 991px) {
+  /* xsmall-1 */
+  @media screen and (max-width: 600px) and (min-width: 481px) {
+
+  }
+
+  /* small-0 */
+  @media screen and (max-width: 840px) and (min-width: 601px) {
+
+  }
+
+  /* small-1 */
+  @media screen and (max-width: 960px) and (min-width: 841px) {
     body {
       padding-top: 33px;
     }
   }
 
-  @media (max-width: 1199px) {
+  /* medium */
+  @media screen and (max-width: 1280px) and (min-width: 961px) {
     body {
       padding-top: 52px;
     }  
+  }
+
+  /* large-0 */
+  @media screen and (max-width: 1440px) and (min-width: 1281px) {
+
+  }
+
+  /* large-1 */
+  @media screen and (max-width: 1600px) and (min-width: 1441px) {
+
+  }
+
+  /* large-2 */
+  @media screen and (max-width: 1920px) and (min-width: 1601px) {
+
+  }
+
+  /* xlarge */
+  @media screen and (min-width: 1921px) {
+
   }
 `;
