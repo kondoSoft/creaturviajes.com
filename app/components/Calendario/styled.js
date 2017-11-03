936697037922f504ae34dec5c10a006e127fbfad
JoJoCoJo <jojocojo@msn.com>
2017-11-02 18:02:47 -0600

    Added styles to calendar

--- app/components/Calendario/styled.js
+++ app/components/Calendario/styled.js
@@ -8,11 +8,18 @@ const DivCalendario = styled.div`
 			font-size: 32px;
 			color: white;
 		}
-
 	}
 
-	.rw-calendar-body{
+	.rw-calendar-transition-group{
+		border-left: 2px solid #8f8f8f;
+		border-right: 2px solid #8f8f8f;
+		border-bottom: 2px solid #8f8f8f;
+	}
 
+	.rw-cell.rw-state-selected, .rw-cell.rw-state-selected:hover{
+		background-color: #ee7202;
+		border: 1px solid #ee7202;
+		font-weight: bold;
 	}
 `;
 
