d94719681b1ee232aab24f03db257917d88588ab
JoJoCoJo <jojocojo@msn.com>
2017-11-06 12:54:02 -0600

    Added responsive for Menu component

--- app/components/Menu/styled.js
+++ app/components/Menu/styled.js
@@ -24,30 +24,9 @@ const NavbarBlue = styled.nav`
                justify-content: flex-end;
   padding-top: 0.5rem;
   padding-bottom: 0.5rem;
-  padding-right: 7%;
   width: 100%;
   color: white;
   background-color: #001a5a;
-
-  /* xsmall-0 */
-  @media screen and (max-width: 480px) {
-
-  }
-
-  /* xsmall-1 */
-  @media screen and (max-width: 600px) and (min-width: 481px) {
-
-  }
-
-  /* small-0 */
-  @media screen and (max-width: 840px) and (min-width: 601px) {
-
-  }
-
-  /* small-1 */
-  @media screen and (max-width: 960px) and (min-width: 841px) {
-
-  }
 
   /* medium */
   @media screen and (max-width: 1280px) and (min-width: 961px) {
@@ -56,24 +35,18 @@ const NavbarBlue = styled.nav`
 
   /* large-0 */
   @media screen and (max-width: 1440px) and (min-width: 1281px) {
-
+    padding-right: 4%;
   }
 
   /* large-1 */
   @media screen and (max-width: 1600px) and (min-width: 1441px) {
-
+    padding-right: 3.5%;
   }
 
   /* large-2 */
   @media screen and (max-width: 1920px) and (min-width: 1601px) {
-
+    padding-right: 7.5%;
   }
-
-  /* xlarge */
-  @media screen and (min-width: 1921px) {
-
-  }
-  
 `;
 
 const NavbarWhite = styled.nav`
@@ -111,34 +84,54 @@ const ButtonNavBar = styled.a`
   transition: all 0.15s ease-in-out;
   color: ${props => props.blue ? 'white' : 'black'};
   background-color: ${props => props.blue ? '#001a5a' : 'white'};
-  margin-right: ${props => props.logo ? '7%' : ''};
   border-color: #001a5a;
   display: ${props => props.displayNone ? 'none' : 'inherit'};
+
   &:hover{
     opacity: 0.5;
   }
 
-
-  @media (max-width: 575px) {
-
-  }
-
-  @media (max-width: 767px) {
+  /* xsmall-0 */
+  @media screen and (max-width: 480px) {
 
   }
 
-  @media (max-width: 991px) {
-    padding: 5px 12px 5px 12px;
-    font-size: 14px;
-    margin-right: ${props => props.logo ? '0px' : ''};
-
+  /* xsmall-1 */
+  @media screen and (max-width: 600px) and (min-width: 481px) {
 
   }
 
-  @media (max-width: 1199px) {
-  padding: 5px 12px 5px 12px;
-  font-size: 14px;
-  margin-right: ${props => props.logo ? '3%' : ''}; 
+  /* small-0 */
+  @media screen and (max-width: 840px) and (min-width: 601px) {
+
+  }
+
+  /* small-1 */
+  @media screen and (max-width: 960px) and (min-width: 841px) {
+  }
+
+  /* medium */
+  @media screen and (max-width: 1280px) and (min-width: 961px) {
+    padding: 10px 15px 10px 15px;
+    font-size: 15px;
+    ${props => props.logo ? 'margin-right: 10%;' : ''}
+  }
+
+  /* large-0 */
+  @media screen and (max-width: 1440px) and (min-width: 1281px) {
+    padding: 10px 15px 10px 15px;
+    font-size: 15px;
+    ${props => props.logo ? 'margin-right: 13%;' : ''}
+  }
+
+  /* large-1 */
+  @media screen and (max-width: 1600px) and (min-width: 1441px) {
+    ${props => props.logo ? 'margin-right: 7%;' : ''}
+  }
+
+  /* large-2 */
+  @media screen and (max-width: 1920px) and (min-width: 1601px) {
+    ${props => props.logo ? 'margin-right: 10%;' : ''}
   }
 `;
 
@@ -159,30 +152,55 @@ const ButtonNavBarWhite = styled(ButtonNavBar)`
 `;
 
 const IconNavBar = styled.img`
-  width: ${props => props.logo ? '254px' : '27px'};
-  height: ${props => props.logo ? '80px' : '27px;'};
-  margin-right: 15px;  
 
-
-  @media (max-width: 575px) {
+  /* xsmall-0 */
+  @media screen and (max-width: 480px) {
 
   }
 
-  @media (max-width: 767px) {
+  /* xsmall-1 */
+  @media screen and (max-width: 600px) and (min-width: 481px) {
 
   }
 
-  @media (max-width: 991px) {
-    width: ${props => props.logo ? '127px' : '14px'};
-    height: ${props => props.logo ? '50px' : '14px;'}
-    margin-right: 15px;  
+  /* small-0 */
+  @media screen and (max-width: 840px) and (min-width: 601px) {
+
   }
 
-  @media (max-width: 1199px) {
-      width: ${props => props.logo ? '180px' : '14px'};
-      height: ${props => props.logo ? '60px' : '14px;'}
-      margin-right: 15px;      
+  /* small-1 */
+  @media screen and (max-width: 960px) and (min-width: 841px) {
+
   }
+
+  /* medium */
+  @media screen and (max-width: 1280px) and (min-width: 961px) {
+    width: ${props => props.logo ? '150px' : '16px'};
+    height: ${props => props.logo ? '35px' : '16px'};
+    margin-right: 6px;
+  }
+
+  /* large-0 */
+  @media screen and (max-width: 1440px) and (min-width: 1281px) {
+    width: ${props => props.logo ? '180px' : '16px'};
+    height: ${props => props.logo ? '40px' : '16px'};
+    margin-right: 8px;
+  }
+
+  /* large-1 */
+  @media screen and (max-width: 1600px) and (min-width: 1441px) {
+    width: ${props => props.logo ? '220px' : '18px'};
+    height: ${props => props.logo ? '50px' : '18px'};
+    margin-right: 15px;
+  }
+
+  /* large-2 */
+  @media screen and (max-width: 1920px) and (min-width: 1601px) {
+    width: ${props => props.logo ? '280px' : '18px'};
+    height: ${props => props.logo ? '60px' : '18px'};
+    margin-right: 15px;
+  }
+
 `;
 
 export { 
