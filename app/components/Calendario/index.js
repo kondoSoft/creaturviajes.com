208fb92d4fdb611f1426b42370ee643a77aa2f80
JoJoCoJo <jojocojo@msn.com>
2017-11-27 12:03:10 -0600

    Clean project from widgets, starting to create own calendar

--- app/components/Calendario/index.js
+++ app/components/Calendario/index.js
@@ -7,37 +7,14 @@
 import React from 'react';
 import { DivCalendario } from './styled';
 
-import Moment from 'moment';
-import 'moment/locale/es.js';
-import momentLocalizer from 'react-widgets-moment';
-
-import { Calendar } from 'react-widgets';
-import 'react-widgets/dist/css/react-widgets.css';
 
 class Calendario extends React.Component {
 
   render(){
-    momentLocalizer(Moment())
 
 	return (
 		<DivCalendario>
-      <Calendar
-        views={['year']}
-        min={new Date(2017, 0)}
-        max={new Date(2018, 11)}
-        footer={false}
-        monthFormat={
-          mt => [
-                  'Ene', 'Feb', 'Mar',
-                  'Abr', 'May', 'Jun',
-                  'Jul', 'Ago', 'Sep',
-                  'Oct', 'Nov', 'Dic'
-                ][
-                  mt.getMonth()
-                ]
-              }
-        currentDateRange={[new Date(2017, 2), new Date(2017, 3)] }
-      />
+        
 		</DivCalendario>
 	)
 
--- app/components/Calendario/styled.js
+++ app/components/Calendario/styled.js
@@ -1,29 +1,7 @@
 import styled from 'styled-components';
 
 const DivCalendario = styled.div`
-	.rw-calendar-header{
-		background:  #ee7202;
-
-		:nth-child(1), :nth-child(2), :nth-child(3) {
-			font-size: 32px;
-			color: white;
-		}
-	}
-
-	.rw-calendar-transition-group{
-		border-left: 1px solid #8f8f8f;
-		border-right: 1px solid #8f8f8f;
-		border-bottom: 1px solid #8f8f8f;
-	}
-
-	.rw-cell.rw-state-selected, .rw-cell.rw-state-selected:hover{
-		background-color: #FDA759;
-		border: 1px solid #FDA759;
-		font-weight: bold;
-	}
-	.rw-cell.rw-state-focus, .rw-cell.rw-state-focus:hover{
-		border: 1px solid #FDA759;
-	}
+	
 `;
 
 
