a732b15dbcf0704d0144930410ea0a88b2d1e850
JoJoCoJo <jojocojo@msn.com>
2017-11-27 16:12:11 -0600

    Calendar v1

--- app/components/Calendario/index.js
+++ app/components/Calendario/index.js
@@ -12,9 +12,46 @@ class Calendario extends React.Component {
 
   render(){
 
+    const meses = [
+                    'Ene', 'Feb', 'Mar',
+                    'Abr', 'May', 'Jun',
+                    'Jul', 'Ago', 'Sep',
+                    'Oct', 'Nov', 'Dic'
+                  ]
+
+  function cambio(msj){
+    var CalendarioCuerpo = document.getElementsByClassName('Body')
+
+    console.log('================'+msj+'====================')
+    console.log(msj)
+    console.log(CalendarioCuerpo)
+  }
 	return (
 		<DivCalendario>
-        
+        <div className='Main'>
+          <div className='Header'>
+            <div id='FlechaIzquierda'>
+              <div className='FlechaIzq' onClick={() => cambio('Izquierda')} ></div>
+            </div>
+            <div className='Titulo'>2017</div>
+            <div id='FlechaDerecha'>
+              <div className='FlechaDer' onClick={() => cambio('Derecha')} ></div>
+            </div>
+          </div>
+          <div className='Body'>
+            {
+              meses.map(
+                (item, i)=>{
+                  return(
+                    <div className='Mes' key={i}>{item}</div>
+                  )
+                }
+              )
+            }
+          </div>
+          <div className='Body oculto'>
+          </div>
+        </div>
 		</DivCalendario>
 	)
 
--- app/components/Calendario/styled.js
+++ app/components/Calendario/styled.js
@@ -1,7 +1,199 @@
 import styled from 'styled-components';
 
 const DivCalendario = styled.div`
-	
+	*{ font-family: AvenirBook; }
+
+	.Main{
+		width: 100%;
+		height: 100%;
+	}
+
+	.Header{
+		display: -webkit-flex;
+		display: flex;
+		-webkit-flex-flex-direction: row;
+					 flex-direction: row;
+		background-color: #ee7202;
+		color: white;
+		.Titulo{
+			width: 70%;
+			font-size: 28px;
+			text-align: center;
+		}
+		#FlechaIzquierda, #FlechaDerecha{
+			width: 15%;
+			display: flex;
+			justify-content: space-around;
+			align-items: center;
+		}
+		.FlechaIzq{
+			width: 0; 
+		    height: 0; 
+		    border-right: 20px solid #ffffff;
+		    border-top: 10px solid transparent;
+		    border-bottom: 10px solid transparent; 
+		}
+		.FlechaDer{
+			width: 0; 
+		    height: 0; 
+		    border-left: 20px solid #ffffff;
+		    border-top: 10px solid transparent;
+		    border-bottom: 10px solid transparent; 
+		}
+	}
+
+	.Body{
+		display: -webkit-flex;
+		display: flex;
+		flex-direction: row;
+		flex-wrap: wrap;
+		justify-content: space-around;
+		align-content: stretch;
+		width: 100%;
+		height: 100%;
+		background-color: white;
+		border-left: 3px solid gray;
+		border-right: 3px solid gray;
+		border-bottom: 3px solid gray;
+
+		.Mes{
+			cursor: pointer;
+			display: -webkit-flex;
+			display: flex;
+			-webkit-flex-justify-content: space-around;
+						 justify-content: space-around;
+			-webkit-flex-align-items: center;
+						 align-items: center;
+			width: 25%;
+			margin-top: 7%;
+			margin-bottom: 7%;
+			&:hover{
+				background-color: #F4AA67;
+			}
+		}
+	}
+	.oculto{
+		display: none;
+	}
+
+
+	.slide-izquierda-entrada {
+		-webkit-animation: slide-izquierda-entrada 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
+		        animation: slide-izquierda-entrada 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
+	}
+
+	@-webkit-keyframes slide-izquierda-entrada {
+	  0% {
+	    -webkit-transform: translateX(-1000px);
+	            transform: translateX(-1000px);
+	    opacity: 0;
+	  }
+	  100% {
+	    -webkit-transform: translateX(0);
+	            transform: translateX(0);
+	    opacity: 1;
+	  }
+	}
+	@keyframes slide-izquierda-entrada {
+	  0% {
+	    -webkit-transform: translateX(-1000px);
+	            transform: translateX(-1000px);
+	    opacity: 0;
+	  }
+	  100% {
+	    -webkit-transform: translateX(0);
+	            transform: translateX(0);
+	    opacity: 1;
+	  }
+	}
+
+	.slide-derecha-entrada {
+		-webkit-animation: slide-derecha-entrada 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
+		        animation: slide-derecha-entrada 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
+	}
+	@-webkit-keyframes slide-derecha-entrada {
+	  0% {
+	    -webkit-transform: translateX(1000px);
+	            transform: translateX(1000px);
+	    opacity: 0;
+	  }
+	  100% {
+	    -webkit-transform: translateX(0);
+	            transform: translateX(0);
+	    opacity: 1;
+	  }
+	}
+	@keyframes slide-derecha-entrada {
+	  0% {
+	    -webkit-transform: translateX(1000px);
+	            transform: translateX(1000px);
+	    opacity: 0;
+	  }
+	  100% {
+	    -webkit-transform: translateX(0);
+	            transform: translateX(0);
+	    opacity: 1;
+	  }
+	}
+
+	.slide-izquierda-salida {
+		-webkit-animation: slide-izquierda-salida 0.5s cubic-bezier(0.550, 0.085, 0.680, 0.530) both;
+		        animation: slide-izquierda-salida 0.5s cubic-bezier(0.550, 0.085, 0.680, 0.530) both;
+	}
+	@-webkit-keyframes slide-izquierda-salida {
+	  0% {
+	    -webkit-transform: translateX(0);
+	            transform: translateX(0);
+	    opacity: 1;
+	  }
+	  100% {
+	    -webkit-transform: translateX(-1000px);
+	            transform: translateX(-1000px);
+	    opacity: 0;
+	  }
+	}
+	@keyframes slide-izquierda-salida {
+	  0% {
+	    -webkit-transform: translateX(0);
+	            transform: translateX(0);
+	    opacity: 1;
+	  }
+	  100% {
+	    -webkit-transform: translateX(-1000px);
+	            transform: translateX(-1000px);
+	    opacity: 0;
+	  }
+	}
+
+	.slide-derecha-salida {
+		-webkit-animation: slide-derecha-salida 0.5s cubic-bezier(0.550, 0.085, 0.680, 0.530) both;
+		        animation: slide-derecha-salida 0.5s cubic-bezier(0.550, 0.085, 0.680, 0.530) both;
+	}
+	@-webkit-keyframes slide-derecha-salida {
+	  0% {
+	    -webkit-transform: translateX(0);
+	            transform: translateX(0);
+	    opacity: 1;
+	  }
+	  100% {
+	    -webkit-transform: translateX(1000px);
+	            transform: translateX(1000px);
+	    opacity: 0;
+	  }
+	}
+	@keyframes slide-derecha-salida {
+	  0% {
+	    -webkit-transform: translateX(0);
+	            transform: translateX(0);
+	    opacity: 1;
+	  }
+	  100% {
+	    -webkit-transform: translateX(1000px);
+	            transform: translateX(1000px);
+	    opacity: 0;
+	  }
+	}
+
 `;
 
 
