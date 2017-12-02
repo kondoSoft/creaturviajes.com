1fec5b71a7bf77f12c84dec860494120c2571425
JoJoCoJo <jojocojo@msn.com>
2017-12-02 13:04:26 -0600

    Testing new calendar v1

--- app/components/Calendario/index.js
+++ app/components/Calendario/index.js
@@ -7,37 +7,29 @@
 import React from 'react';
 import { DivCalendario } from './styled';
 
-import Moment from 'moment';
-import 'moment/locale/es.js';
-import momentLocalizer from 'react-widgets-moment';
-
-import { Calendar } from 'react-widgets';
-import 'react-widgets/dist/css/react-widgets.css';
+//import Calendar from 'react-calendar';
+import Calendar from 'react-calendar/build/entry.nostyle';
 
 class Calendario extends React.Component {
+  state = {
+    date: new Date(),
+  }
+
+  onChange = date => this.setState({ date })
 
   render(){
-    momentLocalizer(Moment())
 
 	return (
 		<DivCalendario>
       <Calendar
-        views={['year']}
-        min={new Date(2017, 0)}
-        max={new Date(2018, 11)}
-        footer={false}
-        monthFormat={
-          mt => [
-                  'Ene', 'Feb', 'Mar',
-                  'Abr', 'May', 'Jun',
-                  'Jul', 'Ago', 'Sep',
-                  'Oct', 'Nov', 'Dic'
-                ][
-                  mt.getMonth()
-                ]
-              }
-        currentDateRange={[new Date(2017, 2), new Date(2017, 3)] }
-      />
+          onChange={this.onChange}
+          value={this.state.date}
+          view='year'
+          //minDate={new Date(2015)}
+          //maxDate={new Date(2020)}
+          minDetail='year'
+          maxDetail='year'
+        />
 		</DivCalendario>
 	)
 
--- app/components/Calendario/styled.js
+++ app/components/Calendario/styled.js
@@ -1,29 +1,45 @@
 import styled from 'styled-components';
 
 const DivCalendario = styled.div`
-	.rw-calendar-header{
-		background:  #ee7202;
-
-		:nth-child(1), :nth-child(2), :nth-child(3) {
-			font-size: 32px;
-			color: white;
-		}
+	button.react-calendar__navigation__arrow.react-calendar__navigation__prev2-button,
+	button.react-calendar__navigation__arrow.react-calendar__navigation__next2-button{
+		display: none;
 	}
 
-	.rw-calendar-transition-group{
-		border-left: 1px solid #8f8f8f;
-		border-right: 1px solid #8f8f8f;
-		border-bottom: 1px solid #8f8f8f;
+	.react-calendar {
+	  width: 100%;
+	  border-bottom: 1px solid #a0a096;
+	  border-left: 1px solid #a0a096;
+	  border-right: 1px solid #a0a096;
 	}
-
-	.rw-cell.rw-state-selected, .rw-cell.rw-state-selected:hover{
-		background-color: #FDA759;
-		border: 1px solid #FDA759;
-		font-weight: bold;
+	
+	.react-calendar button {
+	  border: 0;
+	  outline: none;
 	}
-	.rw-cell.rw-state-focus, .rw-cell.rw-state-focus:hover{
-		border: 1px solid #FDA759;
+	.react-calendar button:not([disabled]):hover {
+	  cursor: pointer;
 	}
+	.react-calendar__navigation {
+		background: #ee7202;
+		font-size: 36px;
+		color: white;
+		padding-top: 2%;
+		padding-bottom: 2%;
+		border: 0;
+	}
+	.react-calendar__navigation button {
+	  min-width: 44px;
+	  height: 100%;
+	}
+	.react-calendar__navigation button:not([disabled]):hover {
+	  background-color: #e6e6e6;
+	}
+	.react-calendar__year-view .react-calendar__tile,
+	.react-calendar__decade-view .react-calendar__tile,
+	.react-calendar__century-view .react-calendar__tile {
+	  padding: 1em 1em;
+	}	
 `;
 
 
